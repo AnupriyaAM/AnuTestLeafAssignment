9bea30ae3b25a61029cb9c8ef26ddff1c3ff1f5e
AnupriyaAM <anuamutha19@gmail.com>
2025-05-22 14:56:28 +0100

    Update alertFrameAssignment.spec.ts

--- Assignment /Week5/Day1/alertFrameAssignment.spec.ts
+++ Assignment /Week5/Day1/alertFrameAssignment.spec.ts
@@ -1,1 +1,24 @@
+import { chromium, expect, test } from "@playwright/test";
 
+test("Validate Alert & Frame", async ({page}) => {
+    page.on('dialog',alertType =>{ 
+        const typeofAlert=alertType.type()
+        console.log(typeofAlert)
+        console.log(alertType.message())
+        if(typeofAlert=="confirm"){
+        alertType.accept()
+        }else{
+        alertType.dismiss()
+        }
+    })
+
+    await page.goto("https://www.w3schools.com/js/tryit.asp?filename=tryjs_confirm");
+    await page.getByText("Accept all & visit the site").click()
+    //option 1
+    // const allFrame=page.frames()
+    // await allFrame[1].locator("//button[text()='Try it']").click()
+    // await expect(allFrame[1].locator("#demo")).toHaveText('You pressed OK!')
+    //option 2
+    await page.frameLocator("#iframeResult").locator("//button[text()='Try it']").click()
+    await expect(page.frameLocator("#iframeResult").locator("#demo")).toHaveText('You pressed OK!')
+})
