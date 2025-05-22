3094bbb73b99e336b024d7f3ac92e4ae54d7ea8a
AnupriyaAM <anuamutha19@gmail.com>
2025-05-22 14:57:22 +0100

    Update frameAssignment.spec.ts

--- Assignment /Week5/Day1/frameAssignment.spec.ts
+++ Assignment /Week5/Day1/frameAssignment.spec.ts
@@ -1,1 +1,16 @@
+import { chromium, expect, test } from "@playwright/test";
 
+test('Handling alert with input',async({page})=>{
+    // - Interact with the Click Me button inside frame 
+    await page.goto("https://leafground.com/frame.xhtml")
+    // - Assert the text changed after clicking the button 
+    await expect(page.frameLocator("(//iframe)[1]").locator("(//button[text()='Click Me'])[1]")).toHaveText("Click Me")
+    await page.frameLocator("(//iframe)[1]").locator("(//button[text()='Click Me'])[1]").click()
+    await expect(page.frameLocator("(//iframe)[1]").locator("(//button[text()='Hurray! You Clicked Me.'])[1]")).toHaveText("Hurray! You Clicked Me.")
+    // - Get the total count of frames present in the page
+    console.log(page.frames().length)
+    // - Interact with the Click Me button present inside the nested frames
+    await page.frameLocator("(//iframe)[3]").frameLocator("#frame2").locator("(//button[text()='Click Me'])[1]").click()
+    // - Assert the text changed after clicking the button
+    await expect(page.frameLocator("(//iframe)[3]").frameLocator("#frame2").locator("(//button[text()='Hurray! You Clicked Me.'])[1]")).toHaveText("Hurray! You Clicked Me.")
+})
